01912e9760ae8bd9cc011c4d51eb124e31651ecd
Hbourmaud <hugo.bourmaud@ynov.com>
2023-04-02 17:46:16 +0200

    working API

--- API/api.py
+++ API/api.py
@@ -1,27 +1,42 @@
 from flask import Flask, jsonify, request
-import pymongo
+from flask_pymongo import PyMongo
+from datetime import datetime
 
 app = Flask(__name__)
 
-app.config['MONGO_DBNAME'] = ''
+app.config['MONGO_DBNAME'] = 'scraper'
 app.config['MONGO_URI'] = ''
 
-mongo = pymongo(app)
+mongo = PyMongo(app)
+
+@app.route('/help', methods=['GET'])
+def help():
+    return jsonify({'For Your Help' : 
+                    ("/games for all games",
+                    "/games/<name> for one specific game by name",
+                    "/platform/<platform> for game by this platform",
+                    "/userscore/<number 0-10> for game by this userscore",
+                    "/metascore/<number 0-100> for game by this metascore",
+                    "/date/<date m_d_y> for game by this date",)
+                    }
+                    )
 
 @app.route('/games', methods=['GET'])
 def get_all_games():
-    games = mongo.db.games
+    games = mongo.db.listGames
 
     output = []
-
     for q in games.find():
-        output.append({'game' : q['name'], 'date' : q['date'], 'platform' : q['platform'], 'title' : q['title'], 'userscore' : q['userscore'], 'metascore' : q['metascore']})
-
+        try:
+            output.append({'game' : q['name'], 'date' : q['date'], 'platform' : q['platform'], 'title' : q['title'], 'userscore' : q['userscore'], 'metascore' : q['metascore']})
+        except:
+            output.append("Wrong game data")
     return jsonify({'result' : output})
 
-@app.route('/games/<name>', method=['GET'])
-def get_one_framework(name):
-    games = mongo.db.games
+
+@app.route('/games/<name>', methods=['GET'])
+def get_one_game(name):
+    games = mongo.db.listGames
 
     q = games.find_one({'name' : name})
 
@@ -30,6 +45,58 @@ def get_one_framework(name):
     else:
         output = 'No results found for this name'
 
+    return jsonify({'result' : output})
+
+@app.route('/platform/<name>', methods=['GET'])
+def get_all_games_platform(name):
+    games = mongo.db.listGames
+
+    output = []
+    for q in games.find({'platform': name}):
+        try:
+            output.append({'game' : q['name'], 'date' : q['date'], 'platform' : q['platform'], 'title' : q['title'], 'userscore' : q['userscore'], 'metascore' : q['metascore']})
+        except:
+            output.append("Wrong game data")
+    return jsonify({'result' : output})
+
+@app.route('/userscore/<name>', methods=['GET'])
+def get_all_games_userscore(name):
+    name = str(float(name))
+    games = mongo.db.listGames
+
+    output = []
+    for q in games.find({'userscore': name}):
+        try:
+            output.append({'game' : q['name'], 'date' : q['date'], 'platform' : q['platform'], 'title' : q['title'], 'userscore' : q['userscore'], 'metascore' : q['metascore']})
+        except:
+            output.append("Wrong game data")
+    return jsonify({'result' : output})
+
+@app.route('/metascore/<name>', methods=['GET'])
+def get_all_games_metascore(name):
+    games = mongo.db.listGames
+
+    output = []
+    for q in games.find({'metascore': name}):
+        try:
+            output.append({'game' : q['name'], 'date' : q['date'], 'platform' : q['platform'], 'title' : q['title'], 'userscore' : q['userscore'], 'metascore' : q['metascore']})
+        except:
+            output.append("Wrong game data")
+    return jsonify({'result' : output})
+
+@app.route('/date/<name>', methods=['GET'])
+def get_all_games_date(name):
+    games = mongo.db.listGames
+    try:
+        name = datetime.strptime(name, '%m_%d_%y').strftime("%B %d, %Y")
+    except:
+        return jsonify({'result' : "Wrong Format date"})
+    output = []
+    for q in games.find({'date': name}):
+        try:
+            output.append({'game' : q['name'], 'date' : q['date'], 'platform' : q['platform'], 'title' : q['title'], 'userscore' : q['userscore'], 'metascore' : q['metascore']})
+        except:
+            output.append("Wrong game data")
     return jsonify({'result' : output})
 
 if __name__ == '__main__':
