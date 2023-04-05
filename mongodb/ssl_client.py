47dcd08e2062ce9d13cbdb08e34834bbc8448237
Brante <brandon.porte@ynov.com>
2023-04-06 01:31:41 +0200

    [UPDATE]

---
+++ mongodb/ssl_client.py
@@ -0,0 +1,31 @@
+import socket, ssl
+from datetime import date,datetime
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+context.load_cert_chain(certfile="/home/brante/connexion/usefull/certificate.pem", keyfile="/home/brante/connexion/usefull/key.pem")
+
+bindsocket = socket.socket()
+bindsocket.bind(('10.101.1.10', 60000))
+bindsocket.listen(5)
+
+def deal_with_client(connstream):
+    data = connstream.recv(1024)
+    # empty data means the client is finished with us
+    result = ''
+    while data:
+        result += str(data)
+        data = connstream.recv(1024)
+    correctData = result[2:-1]
+    file = open("/home/brante/scraper/"+date.today().strftime("%m%d%y")+"_data.json", "w")
+    file.write(correctData)
+    file.close()
+
+while True:
+    newsocket, fromaddr = bindsocket.accept()
+    connstream = context.wrap_socket(newsocket, server_side=True)
+    try:
+        deal_with_client(connstream)
+    finally:
+        connstream.shutdown(socket.SHUT_RDWR)
+        connstream.close()
+
