269ece4adc7be94aa3176df229087004713aa5a1
Hbourmaud <hugo.bourmaud@ynov.com>
2023-04-06 14:45:08 +0200

    Try something

--- setup_scraper.sh
+++ setup_scraper.sh
@@ -40,3 +40,4 @@ sudo systemctl start scraper
 
 current_date=$(date +"%m_%d_%y_data.json")
 sudo scp -r /opt/scraper/$current_date $nameUserMongoDB@10.101.1.10:/home/$nameUserMongoDB/mongodb
+ssh -t $nameUserMongoDB@10.101.1.10 "mongoimport --host localhost -u mongoadmin -p azerty --authenticationDatabase admin --db scraper --collection listGames --file /home/$nameUserMongoDB/$current_date --jsonArray"
