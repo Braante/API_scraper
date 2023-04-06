f3d94281825b2af654032a8539f1d00066bed1e1
Hbourmaud <hugo.bourmaud@ynov.com>
2023-04-06 16:12:09 +0200

    Fix path

--- mongodb/setup_backup_1.sh
+++ mongodb/setup_backup_1.sh
@@ -14,18 +14,18 @@ name=`cat ~/mongodb/name_backup.txt`
 borg init --encryption=none $name@10.101.1.30:/home/$name/backup
 
 # gestion des droits sur les nouveaux fichiers
-sudo chown mongodb backup.service
-sudo chgrp mongodb backup.service
-sudo chown mongodb backup.timer
-sudo chgrp mongodb backup.timer
-sudo chown mongodb backup.sh
-sudo chgrp mongodb backup.sh
+sudo chown mongodb ~/mongdb/backup.service
+sudo chgrp mongodb ~/mongdb/backup.service
+sudo chown mongodb ~/mongdb/backup.timer
+sudo chgrp mongodb ~/mongdb/backup.timer
+sudo chown mongodb ~/mongdb/backup.sh
+sudo chgrp mongodb ~/mongdb/backup.sh
 
 # déplacement du service, du timer et du script
-sudo mv backup.service /etc/systemd/system/
-sudo mv backup.timer /etc/systemd/system/
+sudo mv ~/mongdb/backup.service /etc/systemd/system/
+sudo mv ~/mongdb/backup.timer /etc/systemd/system/
 sudo mkdir /opt/backup
-sudo mv backup.sh /opt/backup/
+sudo mv ~/mongdb/backup.sh /opt/backup/
 
 # activation (aussi dès le démarrage) du service et du timer
 sudo systemctl start backup.timer
